580a70ebe0fc3af6825923e6315f118ff69ffe15
Gladies1627 <139550218+Gladies1627@users.noreply.github.com>
2025-12-02 17:22:03 +0530

    Update OperatorBookingsTab.js

--- fastx-frontend/src/Pages/Operator/OperatorBookingsTab.js
+++ fastx-frontend/src/Pages/Operator/OperatorBookingsTab.js
@@ -10,7 +10,7 @@ const OperatorBookings = () => {
   useEffect(() => {
     const fetchBookings = async () => {
       try {
-        const res = await fetch("http://localhost:8080/api/operator/bookings", {
+        const res = await fetch("https://fastx-backend-ilxf.onrender.com/api/operator/bookings", {
           headers: { Authorization: `Bearer ${token}` },
         });
         const data = await res.json();
